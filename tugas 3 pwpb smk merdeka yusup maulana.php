986a73a61802303c0b1dd0c43632516e35b7f880
westwoood <69501549+westwoood@users.noreply.github.com>
2020-11-03 20:02:49 +0700

    Add files via upload

---
+++ tugas 3 pwpb smk merdeka yusup maulana.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Belajar extends CI_Controller {
+  
+  function __construct(){
+    parent::__construct();
+    
+  }
+ 
+  public function index(){
+    echo "tes";
+  }
+ 
+  public function halo(){
+    echo "hallo";
+  }
+ 
+}
